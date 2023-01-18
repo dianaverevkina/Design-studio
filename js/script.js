a0b082a54d88cf3379cdcdc63346271bf3193773
dianaverevkina <108188896+dianaverevkina@users.noreply.github.com>
2023-01-18 22:53:17 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,3 +10,39 @@ icon.forEach(function (item) {
       back.classList.toggle('lock');
    }
 });
+
+let swiper = new Swiper('.slider', {
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+
+   keybord: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
+   },
+
+   // slidesPerView: 1,
+
+   watchOverflow: true,
+
+   spaceBetween: 70,
+
+   loop: true,
+
+   loopedSlides: 4,
+
+   speed: 700,
+
+   breakpoints: {
+      320: {
+         slidesPerView: 1,
+         centeredSlides: true,
+      },
+      940: {
+         slidesPerView: 2,
+         centeredSlides: false,
+      },
+   },
+})
